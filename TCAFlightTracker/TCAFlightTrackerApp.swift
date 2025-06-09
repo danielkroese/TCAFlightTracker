8df9377afb3b8bb352811adcc370704a138cdb68
Daniël Kroese <d@nielkroese.nl>
2025-06-09 09:50:03 +0200

    Initial Commit

---
+++ TCAFlightTracker/TCAFlightTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAFlightTrackerApp.swift
+//  TCAFlightTracker
+//
+//  Created by Daniël on 09/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct TCAFlightTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TCAFlightTrackerTests/TCAFlightTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TCAFlightTrackerTests.swift
+//  TCAFlightTrackerTests
+//
+//  Created by Daniël on 09/06/2025.
+//
+
+import Testing
+@testable import TCAFlightTracker
+
+struct TCAFlightTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
